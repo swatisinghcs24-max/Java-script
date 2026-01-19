74fbf9fe51c8c1f35e7fd21fc13f653dbf927fe8
Swati singh <swati.singh.cs24@ggits.net>
2026-01-19 17:39:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-function startcelebration() {
-    // For (  let  i=0; i<20; i++)
-{
-    let balloon = document.createElement("div");
-    balloon.style.left  = Math.random() * 100 + "%";
-    balloon.classList.add("balloon");
-    balloon.style.backgroundColor = getRandomColor();
-    document.getElementById("balloons").appendChild(balloon);
-    setTimeout(() => balloon.remove() , 4000 );
-}
-alert("🎉 Make a Wiah and have the best day ever!");
-}
- 
-function getRandomColor(){
-    const colors = ["#ff4e50", "#f9d423" , "#ff6f61" , "#e1bee7" , "#81d4fa"];
-    return colors [Math,  floor (Math.random()*colors.length)];
-}
+
+// function changeText(event){
+//     console.log(event);
+// let fpara = document.getElementById('fpara');
+// fpara.textContent = "hello swati"
+// }
+
+// let fpara = document.getElementById('fpara');
+// fpara.addEventListener('click', changeText);
+
+// let anchorElement = document.getElementById('fanchor');
+// anchorElement.addEventListener('click',function(event){
+// event.preventDefault();
+// anchorElement.textContent="click done bhai"
+// });
+
+// let para = document.querySelectorAll('p');
+
+// for(let  i=0; i<para.length; i++){
+// let para = para[i];
+// para.addEventListener('click', function(){
+//     alert("you have clicked on para:" +i);
+// })
+// }
+ function alertPara(event) {
+    if(event.target.nodeName === 'SPAN'){
+    alert("you have clicked on para :" + event.target.textContent);}
+ }
+ let mydiv = document.getElementById('wrapper');
+
+ document.addEventListener('click' , alertPara);
