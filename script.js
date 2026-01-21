08eafd5dd0d9de178c2d6c44d1ae28eab23ad058
Swati singh <swati.singh.cs24@ggits.net>
2026-01-21 10:42:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-let mydiv = document.querySelector('#mydiv'); 
+// console.log("swati");
+// console.log(math.pi);
 
-let newElement = document.createElement('span');
-newElement.textContent = "swati singh"
-mydiv.insertAdjacentElement('beforebeign', newElement );
-// undefined
-// mydiv
-// <div id=​"mydiv">​…​</div>​
-// let newElement = document.createElement('span');
-// undefined
-// newElement
-// <span> ​</span>​ 
-// newElement.textContent = "this is me swati";
-// 'this is me swati'
-// newElement
-// <span>​this is me swati​</span>​
-// mydiv.insertAdjacentHTML('beforebegin' ,newElement);
 
-// mydiv.insertAdjacentElement('beforebegin' ,newElement);
+//  console.log(Math.PI);
+
+
+// console.log(Math.max(60,30,24,600,712,89));
+
+// console.log(Math.min(60,30,24,600,712,89));
+
+// console.log(Math.round(1.6));
+
+// console.log(Math.floor(1.9));
+
+// console.log(Math.abs(-5));
+
+// console.log(Math.random());
+
+// console.log(Math.sqrt(5));
+
+// console.log(Math.pow(2,10));
+
+
+let curr = new date();
+
+let date = new date('june 20 1998  07:15');
+
+console.log(date);
