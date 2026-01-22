484a8ae3edf94f79b9db4b0f6d083eec051a569a
Swati singh <swati.singh.cs24@ggits.net>
2026-01-22 20:32:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,80 @@
-// console.log("swati");
-// console.log(math.pi);
+// var age = 24;
+
+// console.log(age);
+
+// {
+//     console.log(age);
+// }
+
+// if(true){
+//     console.log(age);
+// }
+
+// function sayhello(){
+
+//     var name = "swati";
+//     console.log("hello duniya", name);
+// }
+
+// sayhello();
+
+// {
+//     var height = 180;
+// }
+// console.log(height);
+
+// let marks = 100;
+// console.log(marks);
 
 
-//  console.log(Math.PI);
+// class human {
+//     age;
+//  #wt = 55;
+//     ht = 200; 
 
+//     constructor(newage ,newheight){
+//            this.age = newage;
+//            this.height = newheight;
+//     }
+//     walking(){
+//         console.log("i am walking" , this.#wt);
+//     }
+//     running(){
+//         console.log("i am running")
+//     }
 
-// console.log(Math.max(60,30,24,600,712,89));
+//     get fetchweight(){
+//         return this.#wt;
+//     }
 
-// console.log(Math.min(60,30,24,600,712,89));
+//     set modifyweight(val){
+//         this.#wt = val ;
+//     }
+// }
 
-// console.log(Math.round(1.6));
+// let obj = new human(50 , 190);
 
-// console.log(Math.floor(1.9));
+// console.log(obj.height);
 
-// console.log(Math.abs(-5));
+// obj.walking();
 
-// console.log(Math.random());
+// function sayname( fname = "uday" , lname = fname.toUpperCase() ){
+//  console.log("my name is:" , fname , " " , lname);
+// }
 
-// console.log(Math.sqrt(5));
+// sayname("swati" );
 
-// console.log(Math.pow(2,10));
+// function solve(value){
+//     console.log("hello jii"  , value);
+// }
 
+// solve(15);
 
-let curr = new date();
+function getage(){
+    return 190;
+}
 
-let date = new date('june 20 1998  07:15');
-
-console.log(date);
+function utility(name = "love" , age = getage()){
+ console.log(name , " " ,age);
+}
+utility();
