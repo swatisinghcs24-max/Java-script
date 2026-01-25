ae85da3a0f414245e677283d535437902c1d981f
Swati singh <swati.singh.cs24@ggits.net>
2026-01-25 18:43:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,86 +1,120 @@
-// function  saymyname(finalname){
-// console.log(finalname);
-// }
-// saymyname("swati");
+// let object ={
+// name : "swati",
+// age : 20 ,
+// weight : 63 ,
+// height : 4.11,
+// greet: function(){ 
+//     console.log("hello jee kaise ho sara");}
+// };  
+// console.log(object);
+// obj.greet();
 
-// var age ;
+// creation of array
 
-// console.log(age);
+// let arr = [1,2,3,4,5];
+// array constructor
+// let arr = new array ('love',1,true);
+// console.log(arr);
 
-// var age = 25 ;
+// let arr =[1,2,3,4,5];
 
-// console.log(age);
+// let brr = new Array('love',1,true);
+// brr.push('swati');
+// brr.pop();
+// brr.shift();
+// brr.unshift('swati');
 
-// let age = 25;
- 
-// sayhello();
-// function sayhello(){
-//     console.log("hello jee kaise hai sara");
-// }
+// brr.push(20);
+// brr.push(30);
+// brr.push(70);
 
-
-// class human{
-
-// }
-
-// const object1 = new human();0
-
-// let greet =  function()  {
-//     console.log("greeting for the day")
-// }
-
-// greet();
+// brr.splice(1,2,'singh');
+// // console.log(brr.slice(2,4));
+// console.log(brr);
 
 
-// function greetme(greet ,fullname)  {
-//     console.log("hello", fullname);
-//     greet();
-// }
-// function greet () {
-//     console.log("greeting for the day")
-// }
 
-// greetme(greet , "swati");
-  
-
-// function solve(number) {
-//     return function(number){
-//         return number*number;
+// let arr = [10,20,30 , 22,11,44,51];
+//  let evenarray = arr.filter((number) => {
+//     if(number % 2 == 0) {
+//         return true;
 //     }
-// }
-//  let ans = solve(5);
-
-//  let finalans = ans(10);
-//  console.log(finalans);
-
-// const arr = [
-//     function(a,b){
-//         return a+b ;
-//     },
-//     function(a,b){
-//       return a-b;
-//     },
-//     function(a,b){
-//         return a*b;
+//     else {
+//         return false;
 //     }
-// ]
+// })
 
-// let first = arr[0];
-// let ans = first(5,10);
+// console.log(evenarray);
 
+
+
+// arr.map((number , index)  => {
+//     console.log(number);
+//     console.log(index);
+// })
+
+//  let ansArray = arr.map((number) => {
+//     return number*number ;
+// })
+// // console.log(ansArray);  
+// let arr = [1,2,'love','swati', null];
+
+//   let ans = arr.filter((value) => {
+//     if(typeof(value) === 'string') {
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }) 
 // console.log(ans);
 
-// let obj ={ 
-//     age:24,
-//     wt:56,
-//     ht:200,
-//     greet : ()=>{
-//          console.log("hello duniya");
+
+// let arr = [10,20,30,40];
+//  let ans = arr.reduce((acc,curr)=>
+// {
+//   return acc + curr;
+// } , 0 );
+
+// console.log(ans);
+ 
+// let arr = [9,1,7,4,2,8];
+// arr.sort();
+
+// console.log(arr);
+// console.log(arr.indexOf(9));
+
+// let arr = [10,20,30];
+
+// let length = arr.length;
+// console.log("lenght:" , length);
+
+// for(let index = 0; index < length; index++){
+// console.log(arr[index]);
+// }
+// arr.forEach((value , index)=> 
+// {
+//     console.log("number:" , value ,"index:", index);
+// })
+
+// let fullname = "swati";
+
+// for(let val of fullname){
+//     console.log(val);
+// }
+
+// let arr = [ 10 ,20, 30 , 40, 50];
+//  function getsum(arr){
+//     let len = arr.length;
+//     let sum = 0;
+//     for(let index=0; index<len; index++){
+//         sum = sum + arr[index];
 //     }
-// } 
+//     return sum;
+//  }
 
+//  let totalsum = getsum(arr);
 
-// console.log(obj.age);
+//  console.log(totalsum);
 
-// obj.greet();
 
